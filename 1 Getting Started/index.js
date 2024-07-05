ed4e90260d6fed63d8a4ca398a6b9812c2a036e2
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-07-06 03:02:58 +0500

    a

---
+++ 1 Getting Started/index.js
@@ -0,0 +1,26 @@
+//   Chapter#1: (Getting Starting)
+
+//(2.2:What is Java Script)
+
+// Four Frequent Questions on java Script
+// 1- What is JS? Its most popular and widely used language in the world right now.
+// With JS we can work as
+// i) Front_End Developer
+// ii) Backend dev
+// iii) Full Stack developer
+
+// 2- What JS can do?
+// Can build Real time world Applications
+// Web/Mobile Apps
+// ii) Real-time networking Apps
+// iii) Command-Line tools
+// iv) Games
+
+// 3- Where JS Code Run?
+// Originally, its run only in browsers, but later its can run on node for backend/programming.
+
+//(5.5: Sepration of Concern)
+//Sepration of Concern? Write java Script in seprate file's.
+
+//(6.6: What is Node?)
+//What is Nide? Node is runtime environment to run Java Script code on system instead of browser.
